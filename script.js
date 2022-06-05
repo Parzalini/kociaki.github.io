df281a102e00efa36fab5008fb056b8cb3e4d83f
Parzalini <106882104+Parzalini@users.noreply.github.com>
2022-06-05 13:16:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+//const hamburger = document.querySelector(".hamburger-lines");
+//const menuLinks = document.querySelector(".menu-links");
+
+//hamburger.addEventListener("click", function () {
+//menuLinks.classList.add("open");
+//});
